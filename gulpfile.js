4c2ba53f05e6bd2a3631112537ea16bb1a18c69b
sanchaman1994 <sanchamansubba16@gmail.com>
2018-01-19 23:37:59 +0530

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
 
-require('./gulp/task/styles');
-require('./gulp/task/watch');
+var gulp = require('gulp'),
+	watch = require('gulp-watch');
+
+	gulp.task('default',function(){
+		console.log("hooray");
+	});
+
+gulp.task('styles',function(){
+	return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+})
+
+gulp.task('watch',function(){
+	watch('./app/index.html', function(){
+		console.log("html changing");
+	});
+});
 
 
-
-
+watch('./app/assets/styles/**/*.css',function(){
+	gulp.start('styles');
+});
 
 
 
