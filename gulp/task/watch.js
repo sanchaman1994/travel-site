693ce4355bc11e8a9e080c7acc3cc8914e15cda5
sanchaman1994 <sanchamansubba16@gmail.com>
2018-01-19 08:53:03 +0530

    added files

---
+++ gulp/task/watch.js
@@ -0,0 +1,29 @@
+
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch',function(){
+	onmouseoutify:false;
+	browserSync.init({
+		server: {
+			baseDir:"app"
+		}
+	});
+
+
+	watch('./app/assets/styles/**/*.css',function(){
+		gulp.start('cssInject');
+	});
+	
+watch('./app/index.html', function(){
+	browserSync.reload();
+});
+
+});
+
+gulp.task('cssInject',['styles'],function(){
+return gulp.src('./app/temp/styles/styles.css')
+	   .pipe(browserSync.stream());
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,15 @@
 
-
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-cssvars = require('postcss-simple-vars'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer');
-
-gulp.task('default', function(){
-	console.log("hello worod");
-});
-
-gulp.task('html', function(){
-	console.log(" imagine sometahing useful being done to your html here.. ");
-});
-
-gulp.task('styles', function(){
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars,autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
+require('./gulp/task/styles');
+require('./gulp/task/watch');
 
 
-});
 
-//watch function
-gulp.task('watch',function(){
 
-	watch('./app/index.html',function(){
 
-		gulp.start('html');
-	});
 
-	watch('./app/assets/styles/**/*.css',function(){
-		gulp.start('styles');
-	});
-});
+
+
+
+
+
+
