550a398a6ce47d1908486583427dbb93db631951
sanchaman1994 <sanchamansubba16@gmail.com>
2018-01-20 02:58:14 +0530

    added

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
 
 var gulp = require('gulp'),
-	watch = require('gulp-watch');
+	watch = require('gulp-watch'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested');
 
 	gulp.task('default',function(){
 		console.log("hooray");
 	});
 
 gulp.task('styles',function(){
-	return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
-})
+	return gulp.src('./app/assets/styles/styles.css')
+			.pipe(postcss([cssvars, autoprefixer]))
+		   .on('error',function(errorInfo){
+		   		console.log(errorInfo.toString());
+		   		this.emit('end');
+		   })
+		   .pipe(gulp.dest('./app/temp/styles'));
+});
 
 gulp.task('watch',function(){
 	watch('./app/index.html', function(){
