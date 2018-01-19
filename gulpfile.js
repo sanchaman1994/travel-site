889d97f7225aca662b8665bcfec0ba09a64e9adc
sanchaman1994 <sanchamansubba16@gmail.com>
2018-01-20 03:52:54 +0530

    update

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ var gulp = require('gulp'),
 
 gulp.task('styles',function(){
 	return gulp.src('./app/assets/styles/styles.css')
-			.pipe(postcss([cssvars, autoprefixer]))
+			.pipe(postcss([cssvars, nested, autoprefixer]))
 		   .on('error',function(errorInfo){
 		   		console.log(errorInfo.toString());
 		   		this.emit('end');
