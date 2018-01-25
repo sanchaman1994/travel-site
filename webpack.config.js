19f68e710e37bdcf2429e85a4b81053ec69cbb26
sanchaman1994 <sanchamansubba16@gmail.com>
2018-01-25 05:57:52 +0530

    comm

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+var path = require('path');
+module.exports = {
+	entry: "./app/assets/scripts/App.js",
+	output: {
+		path : path.resolve(__dirname,"./app/temp/scripts"),
+		filename: "App.js"
+	}
+}
